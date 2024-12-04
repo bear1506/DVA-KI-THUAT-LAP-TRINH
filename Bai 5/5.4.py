8b6d59ea849f299ac70ddd42236092b01ac90e97
Đặng Việt Anh <vietanhbale1506@gmail.com>
2024-12-04 14:28:49 +0700

    Add files via upload

---
+++ Bai 5/5.4.py
@@ -0,0 +1,7 @@
+print("Sv:Đặng Việt Anh")
+print("MSSV:235752021610002")
+##############################
+array= list (range (12, 38))
+print("Mảng được tạo:",array)
+reversed_array= array[::-1]
+print("Mảng được tạo: ", reversed_array)
