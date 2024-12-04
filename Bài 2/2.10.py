cf1477cda5b5da34905e9e66f905194e3347294f
Đặng Việt Anh <vietanhbale1506@gmail.com>
2024-12-04 14:47:24 +0700

    Add files via upload

---
+++ Bài 2/2.10.py
@@ -0,0 +1,8 @@
+print("SV ĐẶNG VIỆT ANH")
+print("MSSV:235752021610002")
+a="hi  i am pythn programmer"
+b=a.split()
+print (b)
+c=" ".join(b)
+print(c)
+
---
+++ Bài 2/2.4.py
@@ -0,0 +1,9 @@
+print("SV ĐẶNG VIỆT ANH")
+print("MSSV:235752021610002")
+############################
+i=1;
+for j in range(2,10):
+    print("i:",i,"j:",j)
+    print(i,"/",j)
+    print(i/j);
+          
