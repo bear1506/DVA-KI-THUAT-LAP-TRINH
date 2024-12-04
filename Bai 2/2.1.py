be78a36e86526593e43016a53caa3f2b0d77e5c1
Đặng Việt Anh <vietanhbale1506@gmail.com>
2024-12-04 14:43:27 +0700

    Add files via upload

--- Bai 2/2.1.py
+++ Bai 2/2.1.py
@@ -1,5 +1,3 @@
-print("SV:ĐẶNG VIỆT ANH")
-print("MSSV: 235752021610002")
 n1=int(input("enter n1 value: "))
 n2=int(input("enter n2 value: "))
 if n1>n2:
@@ -8,3 +6,4 @@ elif n1>n2:
     print("n2 is big")
 else:
     print("Both numbers are equal")
+          
---
+++ Bai 2/2.10.py
@@ -0,0 +1,6 @@
+a="hi  i am pythn programmer"
+b=a.split()
+print (b)
+c=" ".join(b)
+print(c)
+
